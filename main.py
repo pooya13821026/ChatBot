e41dcc23f8084f84a0d4c3f9383785ec2fb2777c
pooya <poria8210@gmail.com>
2026-09-07 20:00:37 +0330

    refactor

--- main.py
+++ main.py
@@ -4,51 +4,58 @@ from openai import OpenAI
 
 dotenv.load_dotenv()
 
-llm = OpenAI(
-    api_key=os.getenv('GROQ_APIKEY'),
-    base_url='https://api.groq.com/openai/v1'
-)
 
-messages = [
-    {'role': 'system', 'content': 'You are Pouya chat bot assistant.'},
-]
+class ChatBot:
+    def __init__(self):
+        self.llm = OpenAI(
+            api_key=os.getenv('GROQ_APIKEY'),
+            base_url='https://api.groq.com/openai/v1'
+        )
 
-total_tokens = 0
+        self.messages = [
+            {'role': 'system', 'content': 'You are Pouya chat bot assistant.'},
+        ]
 
+        self.total_tokens = 0
 
-def bot(message):
-    global total_tokens
+    def bot(self, message):
 
-    messages.append({'role': 'user', 'content': message})
-    response = llm.chat.completions.create(
-        model='qwen/qwen3.8-27b',
-        messages=messages,
-        temperature=0.7,
-        max_tokens=100,
-        # stream=True
-    )
+        self.messages.append({'role': 'user', 'content': message})
+        response = self.llm.chat.completions.create(
+            model='qwen/qwen3.8-27b',
+            messages=self.messages,
+            temperature=0.7,
+            max_tokens=100,
+            # stream=True
+        )
 
-    response_text = response.choices[0].message.content
-    messages.append({'role': 'assistant', 'content': response_text})
+        response_text = response.choices[0].message.content
+        self.messages.append({'role': 'assistant', 'content': response_text})
 
-    total_tokens = total_tokens + response.usage.total_tokens
-    return response_text
+        self.total_tokens = self.total_tokens + response.usage.total_tokens
+        return response_text
 
+    def chat(self):
+        while True:
+            user_input = input("Enter your message (type x to terminate) : ").strip().lower()
 
-def chat():
-    while True:
-        user_input = input("Enter your message (type x to terminate) : ").strip().lower()
+            if user_input == 'x':
+                print("Goodbye!")
+                break
 
-        if user_input == 'x':
-            print("Goodbye!")
-            break
+            print(self.bot(user_input))
 
-        print(bot(user_input))
+    def report(self):
+        for message in self.messages:
+            print(message)
+
+        print(f'total tokens: {self.total_tokens}')
 
 
 def main():
-    chat()
-    print(f'total tokens: {total_tokens}')
+    bot = ChatBot()
+    bot.chat()
+    bot.report()
 
 
 if __name__ == '__main__':
